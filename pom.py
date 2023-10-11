aa1c304aac07404588464f1864e70b20739ca4e1
Pedram Haqiqi <63091219+pedramhaqiqi@users.noreply.github.com>
2023-10-11 18:52:14 -0400

    Include break times

--- pom.py
+++ pom.py
@@ -10,14 +10,21 @@ def signal_handler(signal, frame):
     print("Have a good day!")
     exit(0)
 
-def pomodoro_timer(duration):
+def pomodoro_timer(duration,breakTime):
     print("Pomodoro Timer Started")
-    for i in range(duration, 0, -1):
-        minutes = i // 60
-        seconds = i % 60
-        print(f"Time remaining: {BOLD}{GREEN} {minutes:02d}:{seconds:02d} minutes{RESET}", end='\r')
-        time.sleep(1)
-    print("Pomodoro Timer Completed")
+    while True:
+        for i in range(duration, 0, -1):
+            minutes = i // 60
+            seconds = i % 60
+            print(f"Time remaining: {BOLD}{GREEN} {minutes:02d}:{seconds:02d} minutes{RESET}", end='\r')
+            time.sleep(1)
+        print("Pomodoro Timer Complete, time for a break")
+        for i in range(breakTime, 0, -1):
+            minutes = i // 60
+            seconds = i % 60
+            print(f"Break remaining: {BOLD}{GREEN} {minutes:02d}:{seconds:02d} minutes{RESET}", end='\r')
+            time.sleep(1)
+        print("Break Complete, Next pomodoro...")
 
 def main():
     print("Welcome to Pomodoro Timer")
@@ -25,11 +32,13 @@ def main():
 
     while True:
         action = input("Enter 's' to start the timer with default, 'd' to set duration (mins) default = 25 mins, or 'q' to quit: ")
+
         if action == "d":
             duration = input("Enter Duration: ")
             print("Duration set to: " + duration)
         elif action.lower() == 's':
-            pomodoro_timer(int(duration) * 60)
+            break_time = input("Enter how long breaks are: ")
+            pomodoro_timer(int(duration) * 60, int(break_time) * 60)
         elif action.lower() == 'q':
             print("Thank you for using Pomodoro Timer. Goodbye!")
             break
